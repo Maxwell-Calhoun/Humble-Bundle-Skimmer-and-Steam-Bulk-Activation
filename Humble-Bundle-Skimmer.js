4a146868360fce19a68895bffaa4750116a2877d
Maxwell-Calhoun <mcalh16@gmail.com>
2026-05-24 17:59:44 -0700

    updating humble bundle skimmer to filter out keys that arent steam keys

--- Humble-Bundle-Skimmer.js
+++ Humble-Bundle-Skimmer.js
@@ -1,12 +1,40 @@
-function main() {
-    keys = document.getElementsByClassName("keyfield-value");
-    combinedKeys = '';
-    for (let ii = 0; ii < keys.length; ii++) {
-        if (ii == keys.length - 1) {
-            combinedKeys += keys[ii].innerHTML;
-        } else {
-            combinedKeys += keys[ii].innerHTML + ',';
-        }
-    }
-    console.log(combinedKeys);
+function cleanKeyText(value) {
+  return value
+    .replace(/\s+/g, "")
+    .replace(/&nbsp;/gi, "")
+    .trim()
+    .toUpperCase();
 }
+
+function getKeysFromCurrentPage() {
+  const keyElements = Array.from(
+    document.getElementsByClassName("keyfield-value"),
+  );
+  const keys = [];
+
+  for (let i = 0; i < keyElements.length; i++) {
+    const rawText =
+      keyElements[i].innerText || keyElements[i].textContent || "";
+    const cleanedKey = cleanKeyText(rawText);
+
+    if (/^[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}$/.test(cleanedKey)) {
+      keys.push(cleanedKey);
+    }
+  }
+
+  return keys;
+}
+
+function main() {
+  const keys = getKeysFromCurrentPage();
+  const output = keys.join(",");
+
+  console.log("Keys from current Humble page:");
+  console.log(output);
+
+  prompt("Copy these keys:", output);
+
+  alert(`Done. Found ${keys.length} keys on this page.`);
+}
+
+main();
